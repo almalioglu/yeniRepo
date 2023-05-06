4f2ede87930f51acce070a4ac574d225a91bbea6
almalioglu <almalioglu@gmail.com>
2023-05-06 21:46:32 +0300

    versiyon3

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -2,6 +2,8 @@ public class Class01 {
     public static void main(String[] args) {
         //V1 calismalarim..
         //v2 uzerine calismalar..
+        System.out.println("v3 kodlari...");//v3
+
 
 
     }
